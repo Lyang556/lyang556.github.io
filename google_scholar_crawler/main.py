aa7c5581fbef12233364e04a824ca5e24b25fb03
Lyang556 <53714996+Lyang556@users.noreply.github.com>
2026-06-22 10:22:08 +0800

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -1,44 +1,51 @@
 from scholarly import scholarly, ProxyGenerator
+import logging
+import time
+# from fp.fp import FreeProxy
+# import jsonpickle
 import json
 from datetime import datetime
 import os
-from scholarly._proxy_generator import MaxTriesExceededException
+logging.basicConfig(level=logging.INFO)
 
-# Setup proxy
-pg = ProxyGenerator()
-pg.FreeProxies()  # Use free rotating proxies
-scholarly.use_proxy(pg)
+MAX_RETRIES = 10
+RETRY_DELAY = 60  # seconds
+GOOGLE_SCHOLAR_ID = 'teRf_YAAAAAJ'
 
 
-try:
-    print("正在查找作者信息...")
-    author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
-except MaxTriesExceededException as e:
-    print(f"发生异常: {e}")
-else:
-    print("正在填充作者详细信息...")
-    scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
-    name = author['name']
-    author['updated'] = str(datetime.now())
-    author['publications'] = {v['author_pub_id']:v for v in author['publications']}
-    print(json.dumps(author, indent=2))
-    
-    print("正在创建结果目录...")
-    os.makedirs('results', exist_ok=True)
-    
-    print("正在保存作者数据...")
-    with open(f'results/gs_data.json', 'w') as outfile:
-        json.dump(author, outfile, ensure_ascii=False)
-        
-    print("正在生成 Shields.io 数据...")
-    shieldio_data = {
-      "schemaVersion": 1,
-      "label": "citations",
-      "message": f"{author.get('citedby', 0)}",
-    }
-    
-    print("正在保存 Shields.io 数据...")
-    with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
-        json.dump(shieldio_data, outfile, ensure_ascii=False)
-        
-    print("数据处理完成。")
+for _ in range(MAX_RETRIES):
+    try:
+        logging.info("Script started")
+        # proxy = FreeProxy().get()
+        # logging.info(f"Proxy: {proxy}")
+        pg = ProxyGenerator()
+        pg.FreeProxies()
+        scholarly.use_proxy(pg)
+        author: dict = scholarly.search_author_id(GOOGLE_SCHOLAR_ID)
+        logging.info(f"Success with {_} attempts")
+        break
+    except Exception as e:
+        logging.warning(f"Failed to set sleep time, retrying... {e}")
+        time.sleep(RETRY_DELAY)
+
+scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+name = author['name']
+author['updated'] = str(datetime.now())
+author['publications'] = {v['author_pub_id']:v for v in author['publications']}
+
+os.makedirs('results', exist_ok=True)
+with open(f'results/gs_data.json', 'w') as outfile:
+    json.dump(author, outfile, ensure_ascii=False)
+
+logging.info('Create results/gs_data.json Done')
+
+
+shieldio_data = {
+  "schemaVersion": 1,
+  "label": "citations",
+  "message": f"{author['citedby']}",
+}
+with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
+    json.dump(shieldio_data, outfile, ensure_ascii=False)
+
+logging.info('Create results/gs_data_shieldsio.json Done')
